77113a384d97e708a9ecd3ee988a50fa9bf9ac77
reyshaznii <reyshazni@gmail.com>
2023-12-31 18:02:43 +0700

    feat: update dns

--- src/routes/dns.py
+++ src/routes/dns.py
@@ -60,7 +60,7 @@ async def get_dns_record():
                 "status_code": 500,
                 "status": "failed",
                 "message": "Internal Server Error",
-                "data": {}
+                "data": response.json()
             })
 
     except ValueError as e:
@@ -88,10 +88,58 @@ async def add_dns_record(request: AddDnsRecord):
             "comment": "",
             "tags": []
         }
-
-        response = requests.request("POST", url, json=payload, headers=header)
         
         response = requests.request("POST", url, json=payload, headers=header)
+
+        if response.status_code == 200:
+            return JSONResponse(status_code=200, content={
+                "status_code": 200,
+                "status": "success",
+                "message": "DNS record successfully added",
+                "data": response.json()
+            })
+        elif response.status_code == 400:
+            return JSONResponse(status_code=400, content={
+                "status_code": 400,
+                "status": "failed",
+                "message": "Bad Request",
+                "data": response.json()
+            })
+        else:
+            return JSONResponse(status_code=500, content={
+                "status_code": 500,
+                "status": "failed",
+                "message": "Internal Server Error",
+                "data": response.json()
+            })
+
+    except ValueError as e:
+        return JSONResponse(status_code=401, content={"message": str(e), "data": {}})
+
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"message": str(e), "data": {}})
+
+@dns_router.patch("/")
+async def update_dns_record(record_id: str, request: AddDnsRecord):
+    try:
+        url = "https://api.cloudflare.com/client/v4/zones/" + zone_id + "/dns_records/" + record_id
+
+        header = {
+            "Content-Type": "application/json",
+            "X-Auth-Email": auth_email,
+            "X-Auth-Key": auth_key
+        }
+
+        payload = {
+            "content": request.content,
+            "name": request.name,
+            "proxied": False,
+            "type": request.type,
+            "comment": "",
+            "tags": []
+        }
+        
+        response = requests.request("PATCH", url, json=payload, headers=header)
 
         if response.status_code == 200:
             return JSONResponse(status_code=200, content={
